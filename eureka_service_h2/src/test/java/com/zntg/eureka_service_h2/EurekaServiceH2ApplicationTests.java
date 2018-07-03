26305dc3bcf822d8dd356271088661548a3a0cf3
zhengyunfei <799078779@qq.com>
2018-07-03 10:33:16 +0800

    springClould全家桶教程

---
+++ eureka_service_h2/src/test/java/com/zntg/eureka_service_h2/EurekaServiceH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zntg.eureka_service_h2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaServiceH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ service-ribbon/src/main/java/com/zntg/serviceribbon/HelloControler.java
@@ -0,0 +1,22 @@
+package com.zntg.serviceribbon;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: zntg_clould_v1
+ * @description:
+ * @author: zhengyunfei
+ * @create: 2018-07-03 09:19
+ **/
+@RestController
+public class HelloControler {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
